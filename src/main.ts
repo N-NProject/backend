8def3ce6b9da6e838ed50b8690e4e6eae4a3ed9f
ssojungg <kawy.sojung@gmail.com>
2024-04-04 03:37:02 +0900

    swagger setting

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
 
-  console.log('hello')
+  const config = new DocumentBuilder()
+    .setTitle('Swagger-tutorial')
+    .setDescription('user API description')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('swagger', app, document)
+
+  await app.listen(3000)
 }
 bootstrap()
