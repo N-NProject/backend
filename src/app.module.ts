8c3b03d66c2da8ce06b6601f39bef4cf8f43415c
ssojungg <kawy.sojung@gmail.com>
2024-05-03 02:34:48 +0900

    feat: user 와 board 생성 및 eslint 맞춤

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { BoardModule } from './board/board.module';
+import { UserModule } from './user/user.module';
+import { Board } from './board/entities/board.entity';
+import { User } from './user/entities/user.entitiy';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User, Board], // 엔티티 등록
+      synchronize: true, // 개발 중에는 true로 설정, 프로덕션에서는 false 권장
+    }),
+    BoardModule,
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/board/entities/board.entity.ts
@@ -0,0 +1,50 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '../../user/entities/user.entitiy';
+import { Timestamp } from '../../global/common/timeStamp';
+
+@Entity('boards')
+export class Board extends Timestamp {
+  @PrimaryGeneratedColumn('increment', { name: 'board_id' })
+  @ApiProperty({ description: '게시글 ID' })
+  id: number;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  @ApiProperty({ description: '게시글을 작성한 사용자' })
+  user: User;
+
+  @Column({ name: 'title', type: 'varchar', length: 100, nullable: false })
+  @ApiProperty({ description: '게시글의 제목' })
+  title: string;
+
+  @Column({ name: 'max_capacity', type: 'int', nullable: true })
+  @ApiProperty({ description: '게시글의 최대 참여 인원' })
+  max_capacity: number;
+
+  @Column({ name: 'description', type: 'text', nullable: true })
+  @ApiProperty({ description: '게시글의 설명' })
+  description: string;
+
+  @Column({ name: 'time', type: 'timestamp', nullable: true })
+  @ApiProperty({ description: '게시글의 특정 시간' })
+  time: Date;
+
+  @Column({ name: 'created_at', type: 'timestamp', nullable: true })
+  @ApiProperty({ description: '생성 시간' })
+  created_at: Date;
+
+  @Column({ name: 'updated_at', type: 'timestamp', nullable: true })
+  @ApiProperty({ description: '업데이트 시간' })
+  updated_at: Date;
+
+  @Column({ name: 'deleted_at', type: 'timestamp', nullable: true })
+  @ApiProperty({ description: '삭제 시간' })
+  deleted_at: Date;
+}
