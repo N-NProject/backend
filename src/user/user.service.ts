7ee75e4c59e098b85971ec1daf347ed02cdd0060
non <grapher285@gmail.com>
2024-05-28 23:49:32 +0900

    refactor: 디렉토리 이름 수정, entities

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { User } from './entity/user.entity';
+import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserDto } from './dto/user.dto';
