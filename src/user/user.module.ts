a754ae64d1c4e603795ba0bcb85e52f19547900c
non <grapher285@gmail.com>
2024-05-31 11:47:15 +0900

    fix: 코드 병합 후 문제 해결

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,7 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserService } from './user.service';
 import { User } from './entities/user.entity';
-import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
