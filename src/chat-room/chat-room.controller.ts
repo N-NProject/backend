16e4f8f6bdeaac735bfa3c49f8464c35ab6dece9
suhyeon <suhyeon3484@naver.com>
2024-06-07 00:31:22 +0900

    feat: 채팅방 유저 토큰으로 인증하도록 수정

--- src/chat-room/chat-room.controller.ts
+++ src/chat-room/chat-room.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Delete, Param, Post, Query } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  HttpCode,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { ChatRoomService } from './chat-room.service';
-import { ChatRoom } from './entities/chat-room.entity';
 import { BoardIdDto } from './dto/board-id.dto';
-import { UserIdDto } from './dto/user-id.dto';
 import { ChatRoomIdDto } from './dto/chat-room-id.dto';
+import { AuthGuard } from '../auth/auth.guard';
+import { Token } from '../auth/auth.decorator';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Chat-rooms')
 @Controller('api/v1/chatrooms')
 export class ChatRoomController {
   constructor(private readonly chatRoomService: ChatRoomService) {}
@@ -13,24 +23,28 @@ export class ChatRoomController {
    * 게시글의 채팅방 접속
    */
   @Post('join')
+  @UseGuards(AuthGuard)
+  @HttpCode(200)
   async accessChatRoom(
+    @Token('id') id: number,
     @Query() boardIdDto: BoardIdDto,
-    @Query() userIdDto: UserIdDto,
-  ): Promise<ChatRoom> {
+  ) {
     const chatRoom = await this.chatRoomService.findOrCreateChatRoom(
       boardIdDto.boardId,
     );
-    return this.chatRoomService.joinChatRoom(chatRoom.id, userIdDto.userId);
+    return this.chatRoomService.joinChatRoom(chatRoom.id, id);
   }
 
+  /**
+   * 채팅방 나가기
+   */
   @Delete(':chatRoomId/leave')
+  @UseGuards(AuthGuard)
+  @HttpCode(204)
   async leaveChatRoom(
+    @Token('id') id: number,
     @Param() chatRoomIdDto: ChatRoomIdDto,
-    @Query() userIdDto: UserIdDto,
   ): Promise<void> {
-    return this.chatRoomService.leaveChatRoom(
-      chatRoomIdDto.chatRoomId,
-      userIdDto.userId,
-    );
+    return this.chatRoomService.leaveChatRoom(chatRoomIdDto.chatRoomId, id);
   }
 }
