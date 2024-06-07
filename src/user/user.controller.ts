4162e3ce9a9e09d84a69cc5463541adf1b80be18
suhyeon <suhyeon3484@naver.com>
2024-06-08 01:03:07 +0900

    chore: prettier, eslint에 맞게 수정

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Patch, UseGuards, ValidationPipe, HttpCode } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Patch,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDto } from './dto/user.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
@@ -7,18 +15,21 @@ import { UserResponseDto } from './dto/user.response.dto';
 
 @Controller('api/v1/users')
 export class UserController {
-    constructor(private readonly userService: UserService) { }
+  constructor(private readonly userService: UserService) {}
 
-    @Get()
-    @UseGuards(AuthGuard)
-    getUser(@Token('id') id: number): Promise<UserResponseDto> {
-        return this.userService.getUserById(id);
-    }
+  @Get()
+  @UseGuards(AuthGuard)
+  getUser(@Token('id') id: number): Promise<UserResponseDto> {
+    return this.userService.getUserById(id);
+  }
 
-    @Patch()
-    @HttpCode(204)
-    @UseGuards(AuthGuard)
-    updateUser(@Token('id') id: number, @Body(ValidationPipe) userDto: UserDto): void {
-        this.userService.updateUser(id, userDto);
-    }
+  @Patch()
+  @HttpCode(204)
+  @UseGuards(AuthGuard)
+  updateUser(
+    @Token('id') id: number,
+    @Body(ValidationPipe) userDto: UserDto,
+  ): void {
+    this.userService.updateUser(id, userDto);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,60 +7,60 @@ import { UserResponseDto } from './dto/user.response.dto';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(User)
-        private readonly userRepository: Repository<User>,
-    ) { }
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
-    async getUserByKakaoId(kakaoId: number): Promise<User> {
-        return this.userRepository.findOneBy({ kakaoId });
+  async getUserByKakaoId(kakaoId: number): Promise<User> {
+    return this.userRepository.findOneBy({ kakaoId });
+  }
+
+  async createUserWithKakaoId(kakaoId: number): Promise<User> {
+    const user = this.userRepository.create({ kakaoId });
+    return this.userRepository.save(user);
+  }
+
+  async getUserById(id: number): Promise<UserResponseDto> {
+    const user = await this.userRepository.findOne({
+      where: { id },
+      relations: ['boards'],
+    });
+
+    if (!user) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
     }
 
-    async createUserWithKakaoId(kakaoId: number): Promise<User> {
-        const user = this.userRepository.create({ kakaoId });
-        return this.userRepository.save(user);
+    const createdBoards = user.boards;
+    const joinedBoards = [];
+
+    const userResponseDto: UserResponseDto = {
+      username: user.username,
+      region: user.region,
+      createdBoards,
+      joinedBoards,
+    };
+
+    return userResponseDto;
+  }
+
+  async findOne(id: number): Promise<User> {
+    return this.userRepository.findOneOrFail({ where: { id } });
+  }
+
+  async updateUser(id: number, userDto: UserDto): Promise<void> {
+    const user = await this.userRepository.findOneBy({ id });
+
+    if (!user) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
     }
 
-    async getUserById(id: number): Promise<UserResponseDto> {
-        const user = await this.userRepository.findOne({
-            where: { id },
-            relations: ['boards'],
-        });
+    Object.assign(user, userDto);
 
-        if (!user) {
-            throw new NotFoundException(`Can't find Board with id ${id}`);
-        }
+    this.userRepository.save(user);
+  }
 
-        const createdBoards = user.boards.filter(board => board.user.id === id);
-        const joinedBoards = user.boards.filter(board => board.user.id !== id);
-
-        const userResponseDto: UserResponseDto = {
-            username: user.username,
-            region: user.region,
-            createdBoards,
-            joinedBoards
-        };
-
-        return userResponseDto;
-    }
-  
-    async findOne(id: number): Promise<User> {
-        return this.userRepository.findOneOrFail({ where: { id } });
-    }
-
-    async updateUser(id: number, userDto: UserDto): Promise<void> {
-        const user = await this.userRepository.findOneBy({ id });
-
-        if (!user) {
-            throw new NotFoundException(`Can't find Board with id ${id}`);
-        }
-
-        Object.assign(user, userDto);
-
-        this.userRepository.save(user);
-    }
-
-    async deleteUser(id: number): Promise<void> {
-        this.userRepository.softDelete(id);
-    }
+  async deleteUser(id: number): Promise<void> {
+    this.userRepository.softDelete(id);
+  }
 }
