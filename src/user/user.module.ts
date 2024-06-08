7c9f8f5199510c9b92ad6f6568ad78fb6e8fde0c
non <grapher285@gmail.com>
2024-06-08 21:26:18 +0900

    feat: joinedPosts 반환기능 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,12 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { UserChatRoom } from 'src/user-chat-room/entities/user-chat-room.entity';
+import { ChatRoom } from 'src/chat-room/entities/chat-room.entity';
+import { Board } from 'src/board/entities/board.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, UserChatRoom, ChatRoom, Board])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
