f8404f9ad5885f31d7ebae87f2b153c9cac516f8
ssojungg <kawy.sojung@gmail.com>
2024-06-17 03:36:40 +0900

    fix: token 확인 로그 및 module 수정

--- src/auth/auth.decorator.ts
+++ src/auth/auth.decorator.ts
@@ -1,10 +1,11 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 
 export const Token = createParamDecorator(
-    (data: string, ctx: ExecutionContext) => {
-        const request = ctx.switchToHttp().getRequest();
-        const token = request.token;
+  (data: string, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const token = request.token;
+    console.log(`데코레이션애소의 토근`, token);
 
-        return data ? token?.[data] : token;
-    },
+    return data ? token?.[data] : token;
+  },
 );
--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,29 +1,43 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly jwtService: JwtService) { }
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
-      throw new UnauthorizedException();
+      console.log('토큰이 제공되지 않았습니다.');
+      throw new UnauthorizedException('토큰이 제공되지 않았습니다.');
     }
 
     try {
+      const secret = this.configService.get<string>('JWT_SECRET');
       const payload = await this.jwtService.verifyAsync(token);
+      console.log('토큰 페이로드:', payload);
       request['token'] = payload;
-    } catch {
-      throw new UnauthorizedException();
+    } catch (err) {
+      console.log('유효하지 않은 토큰:', err);
+      throw new UnauthorizedException('유효하지 않은 토큰');
     }
     return true;
   }
 
   private extractTokenFromHeader(request: Request): string | undefined {
     const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    console.log('추출된 토큰:', token);
     return type === 'Bearer' ? token : undefined;
   }
 }
